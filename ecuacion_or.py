b863fc6319c7e8305e536dfa8a9c8fa7658b2e90
DanielFrancoOrtiz <s14120049@alumnos.itsur.edu.mx>
2018-02-22 16:56:43 -0600

    Ecuacion del OR

---
+++ ecuacion_or.py
@@ -0,0 +1,10 @@
+
+def result_or(x,y):
+    if((-1+2*x+2*y)>0):
+        return 1
+    else:
+        return 0
+print(result_or(0,0))
+print(result_or(1,0))
+print(result_or(0,1))
+print(result_or(1,1))
